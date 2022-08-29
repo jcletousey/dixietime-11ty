8ac4ae846d167e813c3a486e34c1523dcd709389
Jean-Charles LE TOUSEY <jcltousey@gmail.com>
2022-08-29 21:35:07 +0200

    Use filter to add nbsp tag between last words of title

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,14 @@ module.exports = function (eleventyConfig) {
   });
 
   // Filters
+  eleventyConfig.addFilter("addNbsp", (str) => {
+    if (!str) {
+      return;
+    }
+    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+    title = title.replace(/"(.*)"/g, '\\"$1\\"');
+    return title;
+  });
   eleventyConfig.addFilter("formatDate", function (str) {
     const date = new Date(str);
     const options = {
